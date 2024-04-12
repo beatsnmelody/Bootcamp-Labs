558ea8f1a71a681e9069f8e1f711a0c927d0b9bd
Alexandra Labaisse <beatsnmelody1@gmail.com>
2024-04-12 13:49:57 -0500

    Finished mortgage and cd

--- Workshops/FinancialCalculatorWorkshop/src/main/java/org/example/Main.java
+++ Workshops/FinancialCalculatorWorkshop/src/main/java/org/example/Main.java
@@ -12,36 +12,144 @@ import java.util.Scanner;
 //User will enter:
 //M = Monthly Payment (double)
 //P = Principal Amount (double)
-//I = Interest Rate (double)
-//N = Number Of Payments (Monthly) (int)
+//I = Interest Rate (double) (Monthly) Calculated by I / 12 then Monthly I / 100 to convert to decimal
+//N = Loan Length (Monthly) (int) (multiply loan length in years by 12)
 //Would display total monthly payment
 //Would also display the total interest the user pays over the lifespan of the loan
 //Mortgage function would take 3 parameters (P, I, N)
 //calculate monthly payment amount by using aforementioned formula
-//calculate total interest by multiplying P * I * N
+//calculate total interest by multiplying M & N and then subtracting by P: (M * N) - P
 //Print results for both monthly payment amount and total interest rate
 
-//CD formula: IB x (1+ (IR / 365)) ^ 365 x ND
+//CD formula: FV = IB x (1+ (IR / 365)) ^ 365 x ND
 //User will enter:
 //IB = Initial balance (double)
-//IR = interest rate (double)
-//ND = Number of years the cd is held
+//IR = interest rate (double) (divide rate by 100 to get decimal)
+//ND = Number of years the cd is held (int)
 //This function will take these three things as parameters as well
 //NOTE: THIS CALCULATOR COMPOUNDS CD INTEREST DAILY!!
 //print results for future value and total interest earned
-//for total interest: TI = IB * IR * ND
+//for total interest: TI = FV (Future Value) - IB
 
 //Present Value Annuity Formula (will probably give this a shot)
-//Present Value (PV) of Annuity = (A ÷ r) (1 – (1 ÷ (1 + r) ^ t))
-//A = Annuity Payment (PER MONTH)
-//T = Number of Periods (in years)
-//R = Yield To Maturity (aka interest rate per year)
+//Present Value (PV) of Annuity = PMT × [(1 - (1 + r)^(-n)) / r]
+//PMT = Annuity Payment (PER MONTH)
+//N = Number of Periods (in years, multiply by 12 to get monthly period)
+//R = interest rate per year (multiply by 12 then divide by 100)
 //NOTE: CONVERT FROM YEARS TO MONTHS WHEN DOING ANNUITY PAYMENT
-//Maybe this will take 4 parameters? (A in months, A in years, T, R)
+//will take 3 parameters (A in months, N, R)
 //Print out results for the present value of annuity
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi lol");
+
+        System.out.println("Welcome to our bank! What is your name?");
+
+        Scanner input = new Scanner(System.in);
+
+        String userName = input.nextLine();
+
+        System.out.printf("So, %s, what calculator do you wish to use? \n", userName);
+
+        System.out.println("""
+                Select calculator:
+                (MORTGAGE) Monthly Payment Amount
+                (CD) Future Value
+                (ANNUITY) Present Value
+                """);
+
+        String command = input.nextLine();
+
+        switch (command.toUpperCase()){
+            case "MORTGAGE":
+                //have user input data
+
+                System.out.println("What will be the principal (loan) amount?");
+                double mPrincipalAmount = input.nextDouble();
+
+                System.out.println("What will be your yearly interest rate?");
+                double mInterestRate = input.nextDouble();
+
+                System.out.println("How many years will you take to pay off this loan?");
+                int mTermInYears = input.nextInt();
+
+                //return monthly payment and total interest function
+
+                double mMonthlyPayment = mortgageMonthlyPayment(mPrincipalAmount, mInterestRate, mTermInYears);
+                double mTotalInterest = mortgageTotalInterest(mPrincipalAmount, mInterestRate, mTermInYears);
+
+                System.out.println(userName + ", the amount you will pay each month is: " + mMonthlyPayment + ", and your total interest on the loan will be: " + mTotalInterest);
+
+                break;
+
+            case "CD":
+                //have user input data
+
+                System.out.println("What will be the initial deposit for the CD?");
+                double cdInitialValue = input.nextDouble();
+
+                System.out.println("What will be the annual interest rate?");
+                double cdInterestRate = input.nextDouble();
+
+                System.out.println("How many years will this CD be invested for?");
+                int cdTermInYears = input.nextInt();
+
+                //return future value of CD and total interest earned function
+
+                double futureValue = cdFutureValue(cdInitialValue, cdInterestRate, cdTermInYears);
+                double totalInterestCDResult = cdTotalInterest(cdInitialValue, cdInterestRate, cdTermInYears);
+
+                System.out.println(userName + ", the future value of your CD will be: " + futureValue + ", and your total interest earned will be: " + totalInterestCDResult);
+
+                break;
+
+            case "ANNUITY":
+                //have user input data
+                //return present value of annuity function
+                //break
+        }
+
+    }
+
+    public static double mortgageMonthlyPayment(double mPrincipalAmount, double mInterestRate, double mTermInYears){
+
+        double mTermInMonths = mTermInYears * 12;
+        double mMonthlyInterestRate = (mInterestRate / 12) / 100;
+
+        double mMonthlyPayment = mPrincipalAmount * (((mMonthlyInterestRate * Math.pow(1 + mMonthlyInterestRate, mTermInMonths))) /  ((Math.pow((1 + mMonthlyInterestRate), mTermInMonths)) - 1));
+
+        return mMonthlyPayment;
+    }
+
+    public static double mortgageTotalInterest(double mPrincipalAmount, double mInterestRate, double mTermInYears){
+
+        double mTermInMonths = mTermInYears * 12;
+        double mMonthlyInterestRate = (mInterestRate / 12) / 100;
+
+        double mMonthlyPayment = mPrincipalAmount * (((mMonthlyInterestRate * Math.pow(1 + mMonthlyInterestRate, mTermInMonths))) /  ((Math.pow((1 + mMonthlyInterestRate), mTermInMonths)) - 1));
+
+        double mTotalInterest = (mMonthlyPayment * mTermInMonths) - mPrincipalAmount;
+
+        return mTotalInterest;
+    }
+
+    public static double cdFutureValue(double cdInitialValue, double cdInterestRate, double cdTermInYears){
+
+        double cdDecimalInterestRate = cdInterestRate / 100;
+
+        double futureValue = cdInitialValue * Math.pow((1 + cdDecimalInterestRate / 365), (365 * cdTermInYears));
+
+        return futureValue;
+    }
+
+    public static double cdTotalInterest(double cdInitialValue, double cdInterestRate, double cdTermInYears){
+
+        double cdDecimalInterestRate = cdInterestRate / 100;
+
+        double futureValue = cdInitialValue * Math.pow((1 + cdDecimalInterestRate / 365), (365 * cdTermInYears));
+
+        double totalInterestCDResult = futureValue - cdInitialValue;
+
+        return totalInterestCDResult;
     }
 }

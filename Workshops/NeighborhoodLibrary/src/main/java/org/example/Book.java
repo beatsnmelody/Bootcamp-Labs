322d7ab0ce6375d343f4fedb004e40d5167ba1ac
Alexandra Labaisse <beatsnmelody1@gmail.com>
2024-04-18 15:22:43 -0500

    rough psuedocode done

---
+++ Workshops/NeighborhoodLibrary/src/main/java/org/example/Book.java
@@ -0,0 +1,17 @@
+package org.example;
+
+//Five properties must be listed: id (int), isbn (string), title (string), isCheckedOut (boolean), checkedOutTo (string)
+//To add some extra spice...bookDescription (string)
+//We need two constructors:
+//one that takes in all 6 parameters
+//one that takes in 4 (excluding isCheckedOut and checkedOutTo)
+//and getters and setters for each one of the 6 parameters
+//also two methods:
+//checkOut (name) and checkIn ()
+//checkOut: is set to the name provided in Library, and the isCheckedOut variable should be true
+//remember: make this applicable to any book
+//checkIn: checkedOutTo should be set to "", and isCheckedOut should be set to false
+
+public class Book {
+
+}
---
+++ Workshops/NeighborhoodLibrary/src/main/java/org/example/Library.java
@@ -0,0 +1,29 @@
+package org.example;
+
+//Library has 4 possible screens
+//Screen Structure
+//After the user enters their name, then the home screen will activate
+//3 Options: Show Available Books, Show Checked Out Books, or Exit
+
+//Option 1: Show Available Books
+//Gets everything EXCEPT DESCRIPTION
+//Prompts the user to either check description or check out the book
+//if description, then get the id, title, and description
+//user can go back to the list once they've read the description
+//if checking out the book, prompt the user for their name
+//then, call the checkOut method
+
+//Option 2: Show Checked Out Books
+//Gets everything except description, again
+//Two options: Check in a book, or go back to the home screen
+//if check in a book, have the user enter the id of the book
+//Check in "Title"? Y/N (get title for this)
+//if yes, goes back to home screen
+//if no, goes back to the list
+//if home screen, goes back to home screen
+
+//Option 3: Exits out of the program
+
+public class Library {
+
+}
